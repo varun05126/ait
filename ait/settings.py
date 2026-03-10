18562b3aa8a6ad34efec11ce221158de7dfe89a2
varun <malthumkarvarun@gmail.com>
2026-03-10 20:17:14 +0530

    Update settings.py

--- ait/settings.py
+++ ait/settings.py
@@ -5,7 +5,7 @@ Clean edition – ready for AI Tourism (AIT)
 
 from pathlib import Path
 import os
-from dotenv import load_dotenv
+from doten import load_dotenv #dotenv
 
 # -------------------- BASE --------------------
 BASE_DIR = Path(__file__).resolve().parent.parent
