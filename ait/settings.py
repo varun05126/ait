da189a9f8b4329e79762c35fd6f5afd38ac1b226
varun <malthumkarvarun@gmail.com>
2026-03-30 21:30:41 +0530

    Update settings.py

--- ait/settings.py
+++ ait/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
 # -------------------- MIDDLEWARE --------------------
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -107,3 +108,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # -------------------- DEFAULT PK --------------------
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
